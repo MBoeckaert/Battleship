6aa50aec419d7e1d7578d8e50549fb8a3e2729ae
EsportsGrandpa <matthiasboeckaert@gmail.com>
2020-01-08 15:19:15 +0100

    Getting guesses
    Adding eventhandler to HTML form

--- script.js
+++ script.js
@@ -12,12 +12,6 @@ var view = {
         cell.setAttribute("class", "miss");
     }
 };
-// view.displayMiss("00");
-// view.displayHit("34");
-// view.displayMiss("55");
-// view.displayMiss("12");
-// view.displayMiss("25");
-// view.displayMessage("tap tap, is this thing on?");
 
 var model = {
     boardSize: 7,
@@ -57,19 +51,6 @@ var model = {
         return true;
     }
 };
-// model.fire("53");
-
-// model.fire("06");
-// model.fire("16");
-// model.fire("26");
-
-// model.fire("34");
-// model.fire("24");
-// model.fire("44");
-
-// model.fire("12");
-// model.fire("11");
-// model.fire("10");
 
 var controller = {
     guesses: 0,
@@ -106,22 +87,17 @@ function parseGuess(guess) {
     }
     return null;
 };
-// console.log(parseGuess("A0"));
-// console.log(parseGuess("B6"));
-// console.log(parseGuess("G3"));
-// console.log(parseGuess("H0"));
-// console.log(parseGuess("A7"));
 
-controller.processGuess("A0");
+function init() {
+    var fireButton = document.getElementById("fireButton");
+    fireButton.onclick = handleFireButton;
+};
 
-controller.processGuess("A6");
-controller.processGuess("B6");
-controller.processGuess("C6");
+function handleFireButton() {
+    var guessInput = document.getElementById("guessInput");
+    var guess = guessInput.value;
+    controller.processGuess(guess);
 
-controller.processGuess("C4");
-controller.processGuess("D4");
-controller.processGuess("E4");
-
-controller.processGuess("B0");
-controller.processGuess("B1");
-controller.processGuess("B2");
+    guessInput.value = "";
+};
+window.onload = init;
